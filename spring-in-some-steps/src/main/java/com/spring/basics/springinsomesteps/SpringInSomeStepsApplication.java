17ad89adb47e6ee4eedbd2b4c968f5925fab4651
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2021-04-15 00:15:22 -0600

    Setting up Spring project using start.spring.io

---
+++ spring-in-some-steps/src/main/java/com/spring/basics/springinsomesteps/SpringInSomeStepsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.basics.springinsomesteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInSomeStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInSomeStepsApplication.class, args);
+	}
+
+}
---
+++ spring-in-some-steps/src/test/java/com/spring/basics/springinsomesteps/SpringInSomeStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.basics.springinsomesteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInSomeStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
