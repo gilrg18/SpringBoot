c90281a33be017b0a38e16d0e48e5a72f039335a
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2021-04-21 00:54:16 -0600

    What is happening in the background?

--- spring-in-some-steps/src/main/java/com/spring/basics/springinsomesteps/BubbleSortAlgorithm.java
+++ spring-in-some-steps/src/main/java/com/spring/basics/springinsomesteps/BubbleSortAlgorithm.java
@@ -1,7 +1,9 @@
 package com.spring.basics.springinsomesteps;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 //@Component to tell spring this is a bean
+@Primary
 @Component
 public class BubbleSortAlgorithm implements SortAlgorithm{
 	//Not really implementing the logic, lets just assume it is implemented...
--- spring-in-some-steps/src/main/java/com/spring/basics/springinsomesteps/QuickSortAlgorithm.java
+++ spring-in-some-steps/src/main/java/com/spring/basics/springinsomesteps/QuickSortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.spring.basics.springinsomesteps;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class QuickSortAlgorithm implements SortAlgorithm{
 	//Not really implementing the logic, lets just assume it is implemented...
 	public int[] sort(int[] numbers) {
