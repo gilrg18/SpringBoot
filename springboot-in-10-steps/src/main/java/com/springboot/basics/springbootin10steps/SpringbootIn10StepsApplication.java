8808c3cce4b4586d9bcc7bcf858e25897c922c20
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2021-04-23 14:16:22 -0600

    Spring Boot Actuator

--- springboot-in-10-steps/src/main/java/com/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ springboot-in-10-steps/src/main/java/com/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -2,17 +2,18 @@ package com.springboot.basics.springbootin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringbootIn10StepsApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ac = SpringApplication.run(SpringbootIn10StepsApplication.class, args);
 		//get all the beans that are present in the application context ac
-		for(String name : ac.getBeanDefinitionNames()) {
-			System.out.println(name);
-		}
+		//for(String name : ac.getBeanDefinitionNames()) {
+			//System.out.println(name);
+		//}
 	}
 
 }
