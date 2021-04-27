aadbcd6861443620426d753af08d31b17474fa21
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2021-04-27 16:42:20 -0600

    Creating User Bean and User Service

--- restful-web-services/src/main/java/com/gil/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/gil/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.gil.rest.webservices.restfulwebservices;
+package com.gil.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
---
+++ restful-web-services/src/main/java/com/gil/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,40 @@
+package com.gil.rest.webservices.restfulwebservices.user;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+	private static List<User> users = new ArrayList<>();
+	//counter to determine id.. 3 current users in our array
+	private static int usersCount = 3;
+	static {
+		users.add(new User(1, "Gil", new Date()));
+		users.add(new User(2, "Maik", new Date()));
+		users.add(new User(3, "Laura", new Date()));
+	}
+	
+	public List<User> findAll(){
+		return users;
+	}
+	
+	public User save(User user) {
+		if(user.getId()==null) {
+			user.setId(++usersCount);
+		}
+		users.add(user);
+		return user;
+	}
+	
+	public User findOne(int id) {
+		for(User user: users) {
+			if(user.getId()==id) {
+				return user;
+			}
+		}
+		return null;
+	}
+}
