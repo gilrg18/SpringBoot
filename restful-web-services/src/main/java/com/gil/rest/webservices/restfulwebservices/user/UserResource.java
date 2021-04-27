72df260045250a4e47a907463aac3863aeed800d
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2021-04-27 16:50:54 -0600

    Implementing GET Methods for User Resource

---
+++ restful-web-services/src/main/java/com/gil/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,27 @@
+package com.gil.rest.webservices.restfulwebservices.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+	
+	@Autowired
+	UserDaoService service;
+	//GET /users
+	//retrieveAllUsers
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers(){
+		return service.findAll();
+	}
+	
+	//retrieveUser(int id)
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		return service.findOne(id);
+	}
+}
