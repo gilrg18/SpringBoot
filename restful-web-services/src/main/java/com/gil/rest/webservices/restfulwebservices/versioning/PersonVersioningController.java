5e2134a6562608dbf1f51fdaa433975be99cfdee
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2021-04-29 19:16:35 -0500

    Versioning RESTful Services - Header and Content Negotiation Approach

--- restful-web-services/src/main/java/com/gil/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ restful-web-services/src/main/java/com/gil/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -6,15 +6,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 	
-	@GetMapping("v1/person")
+	@GetMapping("/v1/person")
 	public PersonV1 personV1() {
 		return new PersonV1("Gilberto");
 	}
 	
-	
-	@GetMapping("v2/person")
+	@GetMapping("/v2/person")
 	public PersonV2 personV2() {
 		return new PersonV2(new Name("Gilberto","Rogel"));
 	}
+	
+	//Another option of doing versioning. Using params
+	@GetMapping(value="/person/param", params="version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Gilberto");
+	}
+	
+	@GetMapping(value="/person/param", params="version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Gilberto","Rogel"));
+	}
+	
+	//Another option of doing versioning. Using produces
+	//we are now not using application/json
+	//we are now appending vnd.ourcompany.app-version+jason
+	@GetMapping(value="/person/produces", produces="application/vnd.company.app-v1+json")
+	public PersonV1 headerV1() {
+		return new PersonV1("Gilberto");
+	}
+	
+	@GetMapping(value="/person/produces", produces="application/vnd.company.app-v2+json")
+	public PersonV2 headerV2() {
+		return new PersonV2(new Name("Gilberto","Rogel"));
+	}
+
+	
 	
 }
