3283c98956d444db51ef480961af71999894d63b
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2021-04-29 22:34:44 -0500

    Creating a JPA Project using SpringInitializr

---
+++ gil-jpa/src/main/java/com/gil/jpa/proyect/GilJpaApplication.java
@@ -0,0 +1,13 @@
+package com.gil.jpa.proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GilJpaApplication.class, args);
+	}
+
+}
---
+++ gil-jpa/src/test/java/com/gil/jpa/proyect/GilJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gil.jpa.proyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GilJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
